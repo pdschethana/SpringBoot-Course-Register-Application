2df63ad361acb7aa4d1378a06aa0165809dd9bd4
pdschethana <supunichethana17@gmail.com>
2025-07-17 21:53:54 +0530

    updated

--- src/main/java/lk/ijse/cmjd110/courseRegisterPro/controller/StudentController.java
+++ src/main/java/lk/ijse/cmjd110/courseRegisterPro/controller/StudentController.java
@@ -10,12 +10,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("api/v1/student")
 public class StudentController {
     // handle student's detail as crud operation
-    // save data
+    // save data students
+
     @PostMapping(consumes = "application/json", produces = "application/json")
 
     //(consumes = MediaType.APPLICATION_JSON_VALUE,Produces=MediaType.APPLICATION_JSON_VALUE)
 
 public ResponseEntity<UserDto> saveStudent(@RequestBody UserDto userDto ){
+
     return new ResponseEntity<>(userDto,HttpStatus.CREATED);
 }
 
