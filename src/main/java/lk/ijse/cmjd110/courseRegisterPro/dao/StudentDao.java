58d54241ce353f791b454c1ed32b753dce7fd443
pdschethana <supunichethana17@gmail.com>
2025-08-08 14:00:32 +0530

    Add basic configs to StudentDao

---
+++ src/main/java/lk/ijse/cmjd110/courseRegisterPro/dao/StudentDao.java
@@ -0,0 +1,10 @@
+package lk.ijse.cmjd110.courseRegisterPro.dao;
+
+import lk.ijse.cmjd110.courseRegisterPro.entities.StudentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentDao extends JpaRepository <StudentEntity, String> {
+
+}
